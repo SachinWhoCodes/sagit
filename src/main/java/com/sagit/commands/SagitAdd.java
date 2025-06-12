ce50291ecd48501d878b4cdce6a78a72c5aa80b2
Sachin Verma <ramasyasachin@gmail.com>
2025-06-13 00:54:44 +0530

    create sagit add

---
+++ src/main/java/com/sagit/commands/SagitAdd.java
@@ -0,0 +1,7 @@
+package com.sagit.commands;
+
+/* Read official "git add" command docs here to understand every options and parameters | Link : https://git-scm.com/docs/git-add */
+
+public class SagitAdd{
+    // will work here in next step
+}
